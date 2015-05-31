009e118821430639cf9b6dd54e20a245ac22847b
lucas <lucas_ismael7@hotmail.com>
2015-05-31 12:57:39 -0300

    Initial

---
+++ ProjetaoExemplo/src/TesteJPane.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+
+public class TesteJPane {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Foi no git");
+	//Adicione o código para me responder caso tenha de certo
+	
+}
+}
